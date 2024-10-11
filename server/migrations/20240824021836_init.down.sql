ccf75c26f3877184c605154e723c8048f89a2a5f
Nick McLean <nickisyourfan@gmail.com>
2024-10-11 06:49:00 -0500

    convert-jobs

---
+++ server/migrations/20240824021836_init.down.sql
@@ -0,0 +1,52 @@
+-- Add down migration script here
+
+-- Drop Entities
+DROP TRIGGER IF EXISTS user_history_trigger;
+DROP TABLE IF EXISTS user_history;
+DROP TABLE IF EXISTS user;
+
+DROP TRIGGER IF EXISTS user_profile_history_trigger;
+DROP TABLE IF EXISTS user_profile_history;
+DROP TABLE IF EXISTS user_profile;
+
+DROP TRIGGER IF EXISTS organization_history_trigger;
+DROP TABLE IF EXISTS organization_history;
+DROP TABLE IF EXISTS organization;
+
+DROP TRIGGER IF EXISTS image_history_trigger;
+DROP TABLE IF EXISTS image_history;
+DROP TABLE IF EXISTS image;
+
+DROP TRIGGER IF EXISTS posting_history_trigger;
+DROP TABLE IF EXISTS posting_history;
+DROP TABLE IF EXISTS posting;
+
+DROP TRIGGER IF EXISTS application_history_trigger;
+DROP TABLE IF EXISTS application_history;
+DROP TABLE IF EXISTS application;
+
+DROP TRIGGER IF EXISTS message_history_trigger;
+DROP TABLE IF EXISTS message_history;
+DROP TABLE IF EXISTS message;
+
+DROP TRIGGER IF EXISTS posting_skill_history_trigger;
+DROP TABLE IF EXISTS posting_skill_history;
+DROP TABLE IF EXISTS posting_skill;
+
+DROP TRIGGER IF EXISTS user_profile_skill_history_trigger;
+DROP TABLE IF EXISTS user_profile_skill_history;
+DROP TABLE IF EXISTS user_profile_skill;
+
+DROP TRIGGER IF EXISTS user_profile_education_history_trigger;
+DROP TABLE IF EXISTS user_profile_education_history;
+DROP TABLE IF EXISTS user_profile_education;
+
+DROP TRIGGER IF EXISTS organization_user_history_trigger;
+DROP TABLE IF EXISTS organization_user_history;
+DROP TABLE IF EXISTS organization_user;
+
+-- Drop References Tables
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS application_status;
+DROP TABLE IF EXISTS skill;
+
