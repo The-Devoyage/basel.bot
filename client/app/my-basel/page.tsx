86de16225506d594afa43eda684f6d936371bd10
Nick McLean <nickisyourfan@gmail.com>
2025-01-05 21:30:00 -0600

    fix table width

--- client/app/my-basel/page.tsx
+++ client/app/my-basel/page.tsx
@@ -35,53 +35,55 @@ const MyBaselPage = async () => {
   return (
     <section className="flex w-full flex-col space-y-4">
       <PageHeader title="My Basel" />
-      <Card>
-        <Accordion>
-          <AccordionPanel>
-            <AccordionTitle>
-              <div className="flex w-full items-center justify-between">
-                <Typography.Heading className="text-2xl">
-                  Shareable Links
-                </Typography.Heading>
-              </div>
-            </AccordionTitle>
-            <AccordionContent>
-              <div className="space-y-3">
-                <Alert color="blue" withBorderAccent>
-                  <div className="mb-3 flex w-full items-start justify-between">
-                    <Typography.Heading className="mt-0 text-lg dark:!text-slate-900">
-                      Getting Noticed
-                    </Typography.Heading>
-                    <CreateShareableLinkButton />
-                  </div>
-                  <Typography.Paragraph className="dark:text-slate-700">
-                    Share your bot with others by simply sharing a link.
-                    Employers and recruiters can converse with your bot about
-                    you, allowing them to quickly match your skills to a
-                    position that is perfect for you.
-                  </Typography.Paragraph>
-                </Alert>
+      <Accordion>
+        <AccordionPanel>
+          <AccordionTitle>
+            <div className="flex w-full items-center justify-between">
+              <Typography.Heading className="text-2xl">
+                Shareable Links
+              </Typography.Heading>
+            </div>
+          </AccordionTitle>
+          <AccordionContent>
+            <div className="space-y-3">
+              <Alert color="blue" withBorderAccent>
+                <div className="mb-3 flex w-full items-start justify-between">
+                  <Typography.Heading className="mt-0 text-lg dark:!text-slate-900">
+                    Getting Noticed
+                  </Typography.Heading>
+                  <CreateShareableLinkButton />
+                </div>
+                <Typography.Paragraph className="dark:text-slate-700">
+                  Share your bot with others by simply sharing a link. Employers
+                  and recruiters can converse with your bot about you, allowing
+                  them to quickly match your skills to a position that is
+                  perfect for you.
+                </Typography.Paragraph>
+              </Alert>
+              <div className="overflow-x-auto">
                 <ShareableLinksTable shareableLinks={shareableLinks.data} />
               </div>
-            </AccordionContent>
-          </AccordionPanel>
-          <AccordionPanel>
-            <AccordionTitle>
-              <div className="flex items-center justify-between">
-                <Typography.Heading className="text-2xl">
-                  Memory Index
-                </Typography.Heading>
-              </div>
-            </AccordionTitle>
-            <AccordionContent>
-              <div className="space-y-3">
-                <ResetIndex />
+            </div>
+          </AccordionContent>
+        </AccordionPanel>
+        <AccordionPanel>
+          <AccordionTitle>
+            <div className="flex items-center justify-between">
+              <Typography.Heading className="text-2xl">
+                Memory Index
+              </Typography.Heading>
+            </div>
+          </AccordionTitle>
+          <AccordionContent>
+            <div className="space-y-3">
+              <ResetIndex />
+              <div className="overflow-x-auto">
                 <UserMetasTable />
               </div>
-            </AccordionContent>
-          </AccordionPanel>
-        </Accordion>
-      </Card>
+            </div>
+          </AccordionContent>
+        </AccordionPanel>
+      </Accordion>
     </section>
   );
 };
